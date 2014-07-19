0bc80996327583c236c8c5e8eb39683b74d1ef94
Andres Pineda <pineda.andres@gmail.com>
2014-07-19 19:26:52 -0400

    Add service value

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,15 @@
  * Controller of the microCarrosApp
  */
 angular.module('microCarrosApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.mensaje = "A lo que rene dijo que sea";
+  .controller('MainCtrl', function ($scope, vehiculos) {
+
+  /*  $scope.mensaje = "A lo que rene dijo que sea";
     $scope.cambiar = function(){
     	$scope.mensaje = $scope.mensaje2;
     }
+  */
+	$scope.vehicules = vehiculos;
+	
+	console.log(vehiculos);
+
   });
---
+++ app/scripts/services/vehiculos.js
@@ -0,0 +1,506 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name microCarrosApp.vehiculos
+ * @description
+ * # vehiculos
+ * Value in the microCarrosApp.
+ */
+angular.module('microCarrosApp')
+  .value('vehiculos', [
+    {
+      "model": {
+        "name": "Model T",
+        "make": "53cad9717ecab70804000006",
+        "_id": "53cada227ecab70804000015",
+        "__v": 0
+      },
+      "year": 1920,
+      "price": 150000,
+      "color": {
+        "name": "Negro",
+        "hex": "000000",
+        "_id": "53cada437ecab70804000016",
+        "__v": 0
+      },
+      "_id": "53cada8f7ecab7080400001e",
+      "__v": 0,
+      "components": [
+        {
+          "name": "mecánico",
+          "_id": "53cad9e27ecab7080400000d",
+          "__v": 0
+        }
+      ],
+      "gallery": [
+        {
+          "path": "",
+          "title": "",
+          "description": "",
+          "_id": "53cada8f7ecab70804000021",
+          "order": 0
+        },
+        {
+          "path": "",
+          "title": "",
+          "description": "",
+          "_id": "53cada8f7ecab70804000020",
+          "order": 0
+        },
+        {
+          "path": "",
+          "title": "",
+          "description": "",
+          "_id": "53cada8f7ecab7080400001f",
+          "order": 0
+        }
+      ]
+    },
+    {
+      "model": {
+        "name": "Model T",
+        "make": "53cad9717ecab70804000006",
+        "_id": "53cada227ecab70804000015",
+        "__v": 0
+      },
+      "year": 1915,
+      "price": 160000,
+      "color": {
+        "name": "Negro",
+        "hex": "000000",
+        "_id": "53cada437ecab70804000016",
+        "__v": 0
+      },
+      "_id": "53cadad27ecab7080400002b",
+      "__v": 0,
+      "components": [
+        {
+          "name": "mecánico",
+          "_id": "53cad9e27ecab7080400000d",
+          "__v": 0
+        }
+      ],
+      "gallery": [
+        {
+          "path": "http://2.bp.blogspot.com/_xEdRg-6CKJM/TA_s7Em4KFI/AAAAAAAAAGw/B5iiBM1IQns/s1600/model-t.gif",
+          "title": "Model T",
+          "description": "Foto del Modelo T cuando salió.",
+          "_id": "53cadad27ecab7080400002e",
+          "order": 0
+        },
+        {
+          "path": "",
+          "title": "",
+          "description": "",
+          "_id": "53cadad27ecab7080400002d",
+          "order": 0
+        },
+        {
+          "path": "",
+          "title": "",
+          "description": "",
+          "_id": "53cadad27ecab7080400002c",
+          "order": 0
+        }
+      ]
+    },
+    {
+      "model": {
+        "name": "Golf",
+        "make": "53cad9c77ecab70804000009",
+        "_id": "53cad9de7ecab7080400000c",
+        "__v": 0
+      },
+      "year": 2005,
+      "price": 300000,
+      "color": {
+        "name": "Azul",
+        "hex": "D92714",
+        "_id": "53cada4b7ecab70804000017",
+        "__v": 0
+      },
+      "_id": "53cadb4f7ecab7080400002f",
+      "__v": 0,
+      "components": [
+        {
+          "name": "aire acondicionado",
+          "_id": "53cad9177ecab70804000002",
+          "__v": 0
+        },
+        {
+          "name": "radio",
+          "_id": "53cad92d7ecab70804000003",
+          "__v": 0
+        },
+        {
+          "name": "ventanas electricas",
+          "_id": "53cad93e7ecab70804000004",
+          "__v": 0
+        },
+        {
+          "name": "automático",
+          "_id": "53cad9e97ecab7080400000e",
+          "__v": 0
+        }
+      ],
+      "gallery": [
+        {
+          "path": "http://lerablog.org/wp-content/uploads/2013/10/2005-VW-Golf-GTI.jpg",
+          "title": "Carro",
+          "description": "Foto Volkswagen Golf 2005",
+          "_id": "53cadb4f7ecab70804000032",
+          "order": 0
+        },
+        {
+          "path": "",
+          "title": "",
+          "description": "",
+          "_id": "53cadb4f7ecab70804000031",
+          "order": 0
+        },
+        {
+          "path": "",
+          "title": "",
+          "description": "",
+          "_id": "53cadb4f7ecab70804000030",
+          "order": 0
+        }
+      ]
+    },
+    {
+      "model": {
+        "name": "Taurus",
+        "make": "53cad9717ecab70804000006",
+        "_id": "53cad9d37ecab7080400000a",
+        "__v": 0
+      },
+      "year": 1954,
+      "price": 125000,
+      "color": {
+        "name": "Negro",
+        "hex": "000000",
+        "_id": "53cada437ecab70804000016",
+        "__v": 0
+      },
+      "_id": "53cadb787ecab70804000034",
+      "__v": 0,
+      "components": [
+        {
+          "name": "aire acondicionado",
+          "_id": "53cad9177ecab70804000002",
+          "__v": 0
+        },
+        {
+          "name": "automático",
+          "_id": "53cad9e97ecab7080400000e",
+          "__v": 0
+        },
+        {
+          "name": "Two-Way Radios & Accessories",
+          "_id": "53cada9c7ecab70804000023",
+          "__v": 0
+        },
+        {
+          "name": "Momo Wheels",
+          "_id": "53cadab37ecab70804000026",
+          "__v": 0
+        }
+      ],
+      "gallery": [
+        {
+          "path": "http://upload.wikimedia.org/wikipedia/commons/c/c2/2010_Ford_Taurus_Limited_2_--_10-31-2009.jpg",
+          "title": "Ford Taurus",
+          "description": "The Ford Taurus is an automobile manufactured by the Ford Motor Company in the United States. Originally introduced in the 1986 model year, it has remained in near-continuous production for more than two decades, making it the fourth-oldest nameplate that is currently sold in the North American Ford lineup.[1] It has had a more-upscale Mercury-branded version entitled the Sable (1986–2005; 2008–2009), as well as a performance variant, the Ford Taurus SHO (1989–1999 and 2010–); in addition, it served as the basis for the first-ever front-wheel drive Lincoln Continental (1988–1994). It was a front-wheel drive mid-size car until 2007, and has been a full-size car since 2008, and available in front- or all-wheel drive since 2008. It has been built on the D3 platform since 2008.\r\n",
+          "_id": "53cadb787ecab70804000037",
+          "order": 0
+        },
+        {
+          "path": "",
+          "title": "",
+          "description": "",
+          "_id": "53cadb787ecab70804000036",
+          "order": 0
+        },
+        {
+          "path": "",
+          "title": "",
+          "description": "",
+          "_id": "53cadb787ecab70804000035",
+          "order": 0
+        }
+      ]
+    },
+    {
+      "model": {
+        "name": "Beetle",
+        "make": "53cad9c77ecab70804000009",
+        "_id": "53cadb737ecab70804000033",
+        "__v": 0
+      },
+      "year": 1970,
+      "price": 50000,
+      "color": {
+        "name": "Azul",
+        "hex": "D92714",
+        "_id": "53cada4b7ecab70804000017",
+        "__v": 0
+      },
+      "_id": "53cadbc67ecab7080400003c",
+      "__v": 0,
+      "components": [
+        {
+          "name": "aire acondicionado",
+          "_id": "53cad9177ecab70804000002",
+          "__v": 0
+        },
+        {
+          "name": "mecánico",
+          "_id": "53cad9e27ecab7080400000d",
+          "__v": 0
+        }
+      ],
+      "gallery": [
+        {
+          "path": "http://www.vwshortbus.com/images/misc_pix/misc_vw_013.jpg",
+          "title": "Cepillo",
+          "description": "Foto de mi sepiyo compra pliz",
+          "_id": "53cadbc67ecab7080400003f",
+          "order": 0
+        },
+        {
+          "path": "",
+          "title": "",
+          "description": "",
+          "_id": "53cadbc67ecab7080400003e",
+          "order": 0
+        },
+        {
+          "path": "",
+          "title": "",
+          "description": "",
+          "_id": "53cadbc67ecab7080400003d",
+          "order": 0
+        }
+      ]
+    },
+    {
+      "model": {
+        "name": "A5",
+        "make": "53cad9de7ecab7080400000b",
+        "_id": "53cada807ecab7080400001c",
+        "__v": 0
+      },
+      "year": 2014,
+      "price": 62000,
+      "color": {
+        "name": "Azul",
+        "hex": "D92714",
+        "_id": "53cada4b7ecab70804000017",
+        "__v": 0
+      },
+      "_id": "53cadc177ecab70804000040",
+      "__v": 0,
+      "components": [
+        {
+          "name": "aire acondicionado",
+          "_id": "53cad9177ecab70804000002",
+          "__v": 0
+        },
+        {
+          "name": "radio",
+          "_id": "53cad92d7ecab70804000003",
+          "__v": 0
+        },
+        {
+          "name": "ventanas electricas",
+          "_id": "53cad93e7ecab70804000004",
+          "__v": 0
+        },
+        {
+          "name": "mecánico",
+          "_id": "53cad9e27ecab7080400000d",
+          "__v": 0
+        },
+        {
+          "name": "automático",
+          "_id": "53cad9e97ecab7080400000e",
+          "__v": 0
+        },
+        {
+          "name": "Nitro",
+          "_id": "53cada547ecab70804000018",
+          "__v": 0
+        },
+        {
+          "name": "Race Communication Systems",
+          "_id": "53cada897ecab7080400001d",
+          "__v": 0
+        }
+      ],
+      "gallery": [
+        {
+          "path": "http://data.supercarros.com/used_cars/361253_main_big.jpg",
+          "title": "Front",
+          "description": "",
+          "_id": "53cadc177ecab70804000043",
+          "order": 0
+        },
+        {
+          "path": "http://data.supercarros.com/used_cars/361253_2109995.jpg",
+          "title": "side",
+          "description": "",
+          "_id": "53cadc177ecab70804000042",
+          "order": 0
+        },
+        {
+          "path": "",
+          "title": "",
+          "description": "",
+          "_id": "53cadc177ecab70804000041",
+          "order": 0
+        }
+      ]
+    },
+    {
+      "model": {
+        "name": "A6",
+        "make": "53cad9de7ecab7080400000b",
+        "_id": "53cad9fe7ecab70804000011",
+        "__v": 0
+      },
+      "year": 2008,
+      "price": 1000000,
+      "color": {
+        "name": "Negro",
+        "hex": "000000",
+        "_id": "53cada437ecab70804000016",
+        "__v": 0
+      },
+      "_id": "53cadc277ecab70804000044",
+      "__v": 0,
+      "components": [
+        {
+          "name": "aire acondicionado",
+          "_id": "53cad9177ecab70804000002",
+          "__v": 0
+        },
+        {
+          "name": "radio",
+          "_id": "53cad92d7ecab70804000003",
+          "__v": 0
+        },
+        {
+          "name": "ventanas electricas",
+          "_id": "53cad93e7ecab70804000004",
+          "__v": 0
+        },
+        {
+          "name": "automático",
+          "_id": "53cad9e97ecab7080400000e",
+          "__v": 0
+        }
+      ],
+      "gallery": [
+        {
+          "path": "http://images.motoring.co.uk/images/car-pictures/mucf-images/500x375/sinc1/21/3b/e5/audi-a6-2008-black-estate-2-7-tdi-le-mans-avant-213be5e48604449a8dfdb1666900fdc5-m2.jpg",
+          "title": "Audi A6",
+          "description": "Barato y bueno! Como nuevo!",
+          "_id": "53cadc277ecab70804000047",
+          "order": 0
+        },
+        {
+          "path": "",
+          "title": "",
+          "description": "",
+          "_id": "53cadc277ecab70804000046",
+          "order": 0
+        },
+        {
+          "path": "",
+          "title": "",
+          "description": "",
+          "_id": "53cadc277ecab70804000045",
+          "order": 0
+        }
+      ]
+    },
+    {
+      "model": {
+        "name": "A5",
+        "make": "53cad9de7ecab7080400000b",
+        "_id": "53cada807ecab7080400001c",
+        "__v": 0
+      },
+      "year": 2014,
+      "price": 62000,
+      "color": {
+        "name": "Azul",
+        "hex": "D92714",
+        "_id": "53cada4b7ecab70804000017",
+        "__v": 0
+      },
+      "_id": "53cadc627ecab70804000048",
+      "__v": 0,
+      "components": [
+        {
+          "name": "aire acondicionado",
+          "_id": "53cad9177ecab70804000002",
+          "__v": 0
+        },
+        {
+          "name": "radio",
+          "_id": "53cad92d7ecab70804000003",
+          "__v": 0
+        },
+        {
+          "name": "ventanas electricas",
+          "_id": "53cad93e7ecab70804000004",
+          "__v": 0
+        },
+        {
+          "name": "mecánico",
+          "_id": "53cad9e27ecab7080400000d",
+          "__v": 0
+        },
+        {
+          "name": "automático",
+          "_id": "53cad9e97ecab7080400000e",
+          "__v": 0
+        },
+        {
+          "name": "Nitro",
+          "_id": "53cada547ecab70804000018",
+          "__v": 0
+        },
+        {
+          "name": "Race Communication Systems",
+          "_id": "53cada897ecab7080400001d",
+          "__v": 0
+        }
+      ],
+      "gallery": [
+        {
+          "path": "http://data.supercarros.com/used_cars/361253_main_big.jpg",
+          "title": "Front",
+          "description": "",
+          "_id": "53cadc627ecab7080400004b",
+          "order": 0
+        },
+        {
+          "path": "http://data.supercarros.com/used_cars/361253_2109995.jpg",
+          "title": "side",
+          "description": "",
+          "_id": "53cadc627ecab7080400004a",
+          "order": 0
+        },
+        {
+          "path": "",
+          "title": "",
+          "description": "",
+          "_id": "53cadc627ecab70804000049",
+          "order": 0
+        }
+      ]
+    }
+  ]);
